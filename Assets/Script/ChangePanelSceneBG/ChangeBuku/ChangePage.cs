1b6985fe13b4d527f09777e07b19706fb76cd5ab
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2025-11-25 08:24:26 +0700

    update kuis

--- Assets/Script/ChangePanelSceneBG/ChangeBuku/ChangePage.cs
+++ Assets/Script/ChangePanelSceneBG/ChangeBuku/ChangePage.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class ChangePage : MonoBehaviour
 {
-    public GameObject[] pages; // Semua halaman buku
+    public GameObject[] pages;
     public Button nextButton;
     public Button backButton;
 
@@ -34,13 +34,11 @@ public class ChangePage : MonoBehaviour
 
     void UpdatePages()
     {
-        // Loop untuk hidup/matikan halaman
         for (int i = 0; i < pages.Length; i++)
         {
             pages[i].SetActive(i == currentPage);
         }
 
-        // Button control
         backButton.interactable = currentPage > 0;
         nextButton.interactable = currentPage < pages.Length - 1;
     }
